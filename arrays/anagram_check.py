b113be9b113b1108a0ce237e36a795717133fb4a
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-02-28 16:39:52 -0500

    completed the changellenge for the function, checking if they are anagrams

--- arrays/anagram_check.py
+++ arrays/anagram_check.py
@@ -15,4 +15,39 @@
   # - check if both lists are equal
 
 def anagram(s1, s2):
-  pass
+  s1 = list(s1.lower().replace(" ",""))
+  s2 = list(s2.lower().replace(" ",""))
+
+  if len(s1) != len(s2):
+    print("This aren't anagrams, not the same length")
+    return
+
+  print(s1)
+  print(s2)
+  
+  print("---------------------------------------------------------------------------")
+
+  # own sort algorithms
+  for i in range(len(s1)-1):
+    for j in range(len(s1)-1):
+      if s1[j] > s1[j+1]:
+        temp = s1[j]
+        s1[j] = s1[j+1]
+        s1[j+1] = temp
+
+      if s2[j] > s2[j+1]:
+        temp = s2[j]
+        s2[j] = s2[j+1]
+        s2[j+1] = temp
+
+  print(s1)
+  print(s2)
+
+  if s1 == s1:
+    print("These strings are anagrams")
+    return
+
+  # print(s1, s2)
+
+
+anagram("Crap built on lies","Public relations")
