5144c6005d5107f2f9b6404d96e032007613dd75
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-02-28 21:32:26 -0500

    refactored solution

--- arrays/anagram_check.py
+++ arrays/anagram_check.py
@@ -10,24 +10,15 @@
 # Ignore spaces and capitalization
 
 # The way I would solve this is would we by doing the following:
-  # - Check the lenght of the string s1 against s2
-  # - if pass, store the charactters with its repetitions on a sorted list within a list = [[a,2], [b,3]...]
-  # - check if both lists are equal
+  # make the strings lists because in python strings are imutable and therefore can't reassign an index for sorting. 
+  # remove spaces and also convert to lowercase
+  # sort the two strings, once sorted return s1 == s2 which will be false or true if they are true anagrams
 
 def anagram(s1, s2):
   s1 = list(s1.lower().replace(" ",""))
   s2 = list(s2.lower().replace(" ",""))
 
-  if len(s1) != len(s2):
-    print("This aren't anagrams, not the same length")
-    return
-
-  print(s1)
-  print(s2)
-  
-  print("---------------------------------------------------------------------------")
-
-  # own sort algorithms
+  # own sort algorithm
   for i in range(len(s1)-1):
     for j in range(len(s1)-1):
       if s1[j] > s1[j+1]:
@@ -40,14 +31,9 @@ def anagram(s1, s2):
         s2[j] = s2[j+1]
         s2[j+1] = temp
 
-  print(s1)
-  print(s2)
+  return s1 == s2
 
-  if s1 == s1:
-    print("These strings are anagrams")
-    return
-
-  # print(s1, s2)
-
-
-anagram("Crap built on lies","Public relations")
+# Testing anagrams and random strings
+print(anagram("Public relations","Crap built on lies"))
+print(anagram("Clint eastwood","old west action"))
+print(anagram("Clint eastwood","old west actionn"))
